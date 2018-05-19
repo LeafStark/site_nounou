8464db7ba330b2afedbbab12b3f450bd87b23f89
leafstark <695549485@qq.com>
2018-05-19 18:55:42 +0200

    addnounouye

---
+++ nounou_ye/nounouRegistration.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>site_nounou</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    </head>
+    <body>
+       
+        <form>
+Login(Email):<br />
+<input type="email" name="login">
+<br />
+Mot de passe:<br />
+<input type="password" name="mot"><br />
+Nom:<br>
+<input type="text" name="nom">
+<br />
+Prenom:
+<br />
+<input type="text" name="prenom">
+<br />
+Age:
+<br />
+<input type="text" name="age">
+<br />
+Portable:<br />
+<input type="tel" name="portable">
+<br />
+Experience:<br />
+<textarea name="experience" row="10" cols="30" ></textarea>
+<br />
+Presentation:<br />
+<textarea name="presentation" row="10" cols="30" ></textarea>
+<br/>
+<input type="submit" value="Submit">
+        </form>
+        
+    </body>
+</html>
