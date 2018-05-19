af2bc038ff2c9afccfcfb4c171cff418ff0c773d
JoJoDU <qiaoqiandu@gmail.com>
2018-05-19 12:12:28 +0200

    regiter.html and addEnfant.js

---
+++ nou/deleteText.js
@@ -0,0 +1,6 @@
+function deleteText(){
+    var table = document.getElementById("ajouteEnfant");
+    var len = table.rows.length;
+    table.deleteRow(len-1);
+}
+
---
+++ nou/register.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script lang="javascript" type="text/javascript" src="addText.js"></script>
+        <script lang="javascript" type="text/javascript" src="deleteText.js"></script>
+    </head>
+    <body>
+        <form method="post" action="parentRegistration.php">
+            <div class="form-group">
+                <input type="text" name="nom" placeholder="Nom de famille" required="true">
+            </div>
+            <div class="form-group">
+                <input type="text" name="ville" placeholder="Ville" required="true">
+            </div>
+            <div class="form-group">
+                <input type="email" name="email" placeholder="Email" required="true">
+            </div>
+            <div class="form-group">
+                <input type="password" name="motDePasse" placeholder="Mot de passe" required="true">
+            </div>
+             <div class="form-group">
+                <input type="text"  name="informationGenerale" placeholder="nformation Générale" required="true">
+            </div>
+            <h2>Ajoute enfant(s)</h2>
+            <table>
+                <tr>
+                    <td>
+                        <table id='ajouteEnfant'>
+                            <tr>
+                                <td>
+                                    <input type="text" name="prenomEnfant" placeholder="Préom d'enfant" required="true">
+                                </td>
+                                <td>
+                                    <input type="text" name="dateNaissance" placeholder="Date de naissance" required="true">
+                                </td>
+                                <td>
+                                    <input type="text" name="restriction" placeholder="Restriction Alimentaire" required="true">
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="button" onclick="addText()" value="Ajouter">
+                        <input type="button" onclick="deleteText()" value="Supprimer">
+                    </td>
+                </tr>
+            </table>
+            <input type="submit" value="S'inscrire">
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
