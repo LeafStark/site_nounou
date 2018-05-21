cb70cd10ccb00dfdd9b8a38a4ed844fd768ac8fb
JoJoDU <qiaoqiandu@gmail.com>
2018-05-21 19:57:10 +0200

    nounou doesn't have php

--- nounou_ye/loginTraitement.php
+++ nounou_ye/loginTraitement.php
@@ -19,14 +19,14 @@ $requet="SELECT * FROM `compte` where `login` = '$email'";
         while($resultat =  $res->fetch()){
             print_r ($resultat);
             if($resultat['mot_de_passe']== $password){
-                echo '2';
+                //echo '2';
             $_SESSION['email']=$email;
             $_SESSION['motDePasse']=$password;
-            $requet_status = "SELECT `status` FROM `compte` WHERE `login`='$email'";
-            $res_status = $dbh->query($requet_status);
-            while($res_s =  $res_status->fetch()){
+            $requet_status = "SELECT `role` FROM `compte` WHERE `login`='$email'";
+            $res_role = $dbh->query($requet_status);
+            while($res_s =  $res_role->fetch()){
             print_r($res_s);
-            $_SESSION['type'] = $res_s['status'];
+            $_SESSION['type'] = $res_s['role'];
             if($_SESSION['type']=="parents"){
                 header('Location:parent.php');
             }
