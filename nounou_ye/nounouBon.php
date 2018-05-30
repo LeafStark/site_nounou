9a22a4f68506b3933b86b5520fe9366c6094631c
JoJoDU <qiaoqiandu@gmail.com>
2018-05-30 16:22:22 +0200

    select nououbon

--- nounou_ye/nounouBon.php
+++ nounou_ye/nounouBon.php
@@ -12,7 +12,25 @@ and open the template in the editor.
     <body>
         <?php
         require 'sauveParentSelection.php';
-        echo("$type");
+        require_once 'pdoConnexion.php';
+        session_start();
+        
+        if ($type == 'Garde ponctuelle') {
+            $query = "SELECT * FROM `nounou` WHERE `type`='Garde ponctuelle'";
+            foreach ($dbh->query($query) as $row) {
+                print_r($row); 
+            }
+        } elseif ($type == 'Garde d’enfant régulière') {
+            $query = "SELECT * FROM `nounou` WHERE `type`='Garde d’enfant régulière'";
+            foreach ($dbh->query($query) as $row) {
+                print_r($row);
+            }
+        } elseif ($type == 'Garde d\'enfant en langues étrangères') {
+            $query = "SELECT * FROM `nounou` WHERE `type`='Garde d\'enfant en langues étrangères'";
+            foreach ($dbh->query($query) as $row) {
+                print_r($row);
+        }
+        }
         ?>
     </body>
 </html>
