caf49d1627c25297c7ce07d829480d5ca194d9b2
JoJoDU <qiaoqiandu@gmail.com>
2018-05-31 21:15:13 +0200

    acceuil hasn't done

--- nounou_ye/acceuil.php
+++ nounou_ye/acceuil.php
@@ -5,14 +5,36 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
+     <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>NouNouPratique</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
     </head>
+    
     <body>
+        <div id="bannerImage">
+               <div class="container">
+                   <center>
+                   <div id="bannerContent">
+                       <h1>Nous sommes NouNouPratique.</h1>
+                       <p>Pour la belle vie.</p>
+                       
+                   </div>
+                   </center>
+               </div>
+           </div>
         <div>
             <a href="signin.html">Login</a>
-            <a href="choixRegistion">Registion</a>
+            <a href="choixRegistion">Registion</a
         </div>
         <?php
         // put your code here
