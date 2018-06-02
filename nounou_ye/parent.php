8c695ac2ac0074d4328664c079d7faa5bcb2f10b
JoJoDU <qiaoqiandu@gmail.com>
2018-06-02 16:21:57 +0200

    effectue has problem

--- nounou_ye/parent.php
+++ nounou_ye/parent.php
@@ -4,29 +4,59 @@ require_once 'pdoConnexion.php';
 
 $email = $_SESSION['email'];
 $requet = "select * from parents where Email = '$email'";
-$resultats = $dbh->query($requet); 
-$row = $resultats->fetch(); 
+$resultats = $dbh->query($requet);
+$requetCherche = "SELECT `type_nounou`, `date_debut`, `date_fin`, `heure_debut`, `heure_fin`, `nb_enfant` FROM `parents_cherche` WHERE email = '$email'";
+$resultatCherche = $dbh->query($requetCherche);
+$requetCommande = "SELECT * FROM `commande` WHERE Email_P = $email";
+$resultatCommande = $dbh->query($requetCommande);
+$row = $resultats->fetch();
+$rowCherche = $resultatCherche->fetch();
+$rowCommande = $resultatCommande->fetch();
 //print_r($row);
-    echo("<h1>Page du ".$row['Nom_F']."<h1>");
-    
-                echo "<pre>";
-                echo "<ul>";
-                echo "<li>Ville: ".$row['Ville']."</li>";
-                echo "<li>Email: ".$row['Email']."</li>";
-                echo "<li>Information: ".$row['Information']."</li>";
-                echo"</ul>";
-               //echo "Valider"."<input type='checkbox' name= 'valider[]' value=".$row['Email'].">";
-              // echo"Refuser"."<input type='checkbox' name= 'refuser[]' value=".$row['Email'].">";
-                echo "</pre>";
-
+//个人信息
+echo("<h1>Page du " . $row['Nom_F'] . "<h1>");
+echo("<div>");
+echo "<pre>";
+echo "<ul>";
+echo "<li>Ville: " . $row['Ville'] . "</li>";
+echo "<li>Email: " . $row['Email'] . "</li>";
+echo "<li>Information: " . $row['Information'] . "</li>";
+echo"</ul>";
+//echo "Valider"."<input type='checkbox' name= 'valider[]' value=".$row['Email'].">";
+// echo"Refuser"."<input type='checkbox' name= 'refuser[]' value=".$row['Email'].">";
+echo "</pre>";
+echo("</div>");
+//print_r($rowCherche);
+//保姆预存信息
+echo("<h1>Votre nounou cherche est: </h1>");
+if ($rowCherche != null) {
+    foreach ($rowCherche as $key => $value) {
+        foreach ($value as $k => $v) {
+            if (!is_integer($k)) {
+                echo("<div>");
+                echo("<ul>");
+                echo("<li>$k : $v</li>");
+                echo("</ul>");
+                echo("</div>");
+                continue;
+            }
+        }
+    }
+} else {
+    echo("Vide!");
+    echo("Cliquer le lien \"Modifier la cherche\" pour le chercher");
+}
 ?>
 <html>
     <body>
         <div>
-           <a href="parentCherche.php">Modifier la cherche</a>
+            <a href="parentCherche.php">Modifier la cherche</a>
         </div>
         <div>
-           <a href="nounouBon.php">Voir les nounous je cherche.</a>
+            <a href="nounouBon.php">Voir les nounous je cherche.</a>
+        </div>
+        <div>
+            echo("<a href='commandeEffctue.php'>Mes commandes</a>");
         </div>
     </body>
 </html>
