61c7187ae7853e62db5de6257694297a5650bcd9
JoJoDU <qiaoqiandu@gmail.com>
2018-06-08 15:18:43 +0200

    commandeeffectue has problem

--- nounou_ye/commandeEffectue.php
+++ nounou_ye/commandeEffectue.php
@@ -6,26 +6,37 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
+        <script src="popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="carousel.css" rel="stylesheet">
     </head>
     <body>
         <?php
         session_start();
         require_once 'pdoConnexion.php';
-
+        require_once 'navbar.html';
         $email = $_SESSION['email'];
         //echo $email;
         //$requetCommande = "select * from `nounou`";
         $requetCommande = "SELECT n.`Nom`, n.`Prenom`, n.`Ville`, n.`Email`, n.`Portable`,n.`Evaluation`,c.`Duree`, c.`Salaire`,c.`Status` FROM `nounou` AS n, `commande` AS c WHERE c.Email_P = '$email'AND c.Email_N=n.`Email`";
         $resultatCommande = $dbh->query($requetCommande);
         $rowCommande = $resultatCommande->fetch();
-        // echo var_dump($rowCommande);
+        //echo var_dump($rowCommande);
         //echo $rowCommande['Email'];
-        if ($rowCommande != null) {
+        if ($rowCommande != null) { 
+            echo("<br>");
             foreach ($rowCommande as $key => $value) {
-
-                if (!is_integer($key)) {
+                if (!is_integer($key) && $key != "Status") {
+                  
                     echo("<div>");
                     echo("<ul>");
                     echo("<li>$key : $value</li>");
@@ -35,8 +46,9 @@ and open the template in the editor.
                     continue;
                 }
             }
-             if ($rowCommande['Status'] == 0 ) {
-            echo <<<STO
+            var_dump($rowCommande['Status']);
+            if ($rowCommande['Status'] == 0) {
+                echo <<<STO
       <form method="post" action="evaluer.php">
             <input type="radio" name="evaluation" value="1"> 1 
            <input type="radio" name="evaluation" value="2"> 2
@@ -45,18 +57,17 @@ and open the template in the editor.
            <input type="radio" name="evaluation" value="5"> 5
 STO;
 
-            $e = $rowCommande['Email'];
-            echo("<input type='hidden' name='emailN' value=$e>");
-            echo(" <input type=\"submit\" value=\"Evaluer\">");
-        } else {
-            echo("Cette commande a été évaluée");
-        }
+                $e = $rowCommande['Email'];
+                echo("<input type='hidden' name='emailN' value=$e>");
+                echo(" <input type=\"submit\" value=\"Evaluer\">");
+            } else {
+                echo("Cette commande a été évaluée");
+            }
         } else {
             echo("Vide!<br/>");
             echo("Cliquer le lien \"Modifier la cherche\" pour le chercher<br />");
-             echo "<a href=\"parentCherche.php\">Modifier la cherche</a>";
+            echo "<a href=\"parentCherche.php\">Modifier la cherche</a>";
         }
-       
         ?>
 
     </form>
--- nounou_ye/evaluer.php
+++ nounou_ye/evaluer.php
@@ -18,14 +18,17 @@ and open the template in the editor.
     $requetEvaA="SELECT `Evaluation` FROM `nounou` WHERE `Email`='$emailN'";
     $resA = $dbh->query($requetEvaA);
    $rowA = $resA->fetch();
- //  $statusEva = $rowA['Status'];
+// $statusEva = $rowA['Status'];
 
     $evaluationNouveau = ($rowA['Evaluation']+$evaluation)/2;
     $requetEva = "UPDATE `nounou` SET `Evaluation`='$evaluationNouveau' WHERE `Email`='$emailN'";
-    $requetEvaStatus= "UPDATE `commande` SET `Status`= 1 WHERE `Email`='$emailN'";
+    $requetEvaStatus= "UPDATE `commande` SET `Status`= 1 WHERE `Email_N`='$emailN'";
     $resultatEva = $dbh->exec($requetEva);
+   // var_dump($resultatEva);
     $resultatEvaStatus = $dbh->exec($requetEvaStatus);
+    //var_dump($resultatEvaStatus);
     echo 'Votre evaluation a déja été enregistrée.';
+    
     ?>
     </body>
 </html>
